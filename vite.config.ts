fa99aa083e8fbc2e85cfea4d69671c697a319e89
khageshpatil <patilkhagesh145@gmail.com>
2026-07-31 00:32:49 +0530

    Deployment fix 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: process.env.VITE_BASE || '/Smart-MD-Viewer/',
   server: {
     host: "::",
     port: 8080,
